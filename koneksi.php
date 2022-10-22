f208d18b4f252379d5029fa3a67ed2f7695b5454
azwarbahar <azwarbahar07@gmail.com>
2022-10-22 17:14:12 +0800

    chnge config db

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 date_default_timezone_set("Asia/Makassar");
 session_start();
-$conn = mysqli_connect("localhost", "root", "", "uinamfind_db");
-// $conn = mysqli_connect("156.67.212.126", "u3737783_uinamfind", "uinamfind", "u3737783_uinamfind_db");
+// $conn = mysqli_connect("localhost", "root", "", "uinamfind_db");
+$conn = mysqli_connect("localhost", "u3737783_uinamfind", "uinamfind", "u3737783_uinamfind_db");
 ?>
 <!-- again -->
 <?php
